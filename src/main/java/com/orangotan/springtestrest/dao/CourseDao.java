661598154cd0fea3f7084a35085b53d34525407e
PratikMaurya <pratikmaurya101@gmail.com>
2024-04-19 18:24:47 +0530

    changed repo from JpaRepo to CrudRepo

--- src/main/java/com/orangotan/springtestrest/dao/CourseDao.java
+++ src/main/java/com/orangotan/springtestrest/dao/CourseDao.java
@@ -2,8 +2,9 @@ package com.orangotan.springtestrest.dao;
 
 import com.orangotan.springtestrest.entity.Course;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface CourseDao extends JpaRepository<Course,Long> {
+public interface CourseDao extends CrudRepository<Course,Long> {
 }
